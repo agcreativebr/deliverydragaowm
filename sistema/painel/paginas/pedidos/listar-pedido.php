007124e70196377f4e0ac4952596f73738f3cd8d
Wesley Mendes <agcreativebr@gmail.com>
2025-07-10 12:35:24 -0300

    fix: remoção de blocos HTML/PHP literais no detalhe do pedido; exibição 100% limpa, sem código quebrado ou PHP literal

--- sistema/painel/paginas/pedidos/listar-pedido.php
+++ sistema/painel/paginas/pedidos/listar-pedido.php
@@ -407,9 +407,7 @@ HTML;
 </div>
 HTML;
 }
-echo <<<HTML
-<div class="th" style="margin-bottom: 7px"></div>
-HTML;
+echo '<div class="th" style="margin-bottom: 7px"></div>';
 
 // Cálculo matemático exato dos totais
 // Subtotal: soma dos produtos (sem taxa, sem frete, sem desconto)
@@ -470,66 +468,54 @@ if (isset($taxa_cartao) && $taxa_cartao > 0 && strtolower($tipo_pgto) == 'cartã
 
 HTML;
 if ($total_pago != $valor) {
-echo <<<HTML
-	<div class="row valores">
-	<div class="col-md-6">Valor Recebido</div>
-	<div class="col-md-6" align="right">R$ {$total_pagoF}</div>
-	</div>
-	HTML;
-	}
+echo '<div class="row valores">';
+	echo '<div class="col-md-6">Valor Recebido</div>';
+	echo '<div class="col-md-6" align="right">R$ ' . $total_pagoF . '</div>';
+	echo '</div>';
+}
 
-	if ($troco > 0) {
-	echo <<<HTML
-		<div class="row valores">
-		<div class="col-md-6">Troco</div>
-		<div class="col-md-6" align="right">R$ {$trocoF}</div>
-		</div>
-		HTML;
-		}
-		echo <<<HTML
-			<div class="th" style="margin-bottom: 7px">
-			</div>
+if ($troco > 0) {
+echo '<div class="row valores">';
+	echo '<div class="col-md-6">Troco</div>';
+	echo '<div class="col-md-6" align="right">R$ ' . $trocoF . '</div>';
+	echo '</div>';
+}
+echo '<div class="th" style="margin-bottom: 7px">
+</div>
 
-			<div class="row valores">
-				<div class="col-md-6">Forma de Pagamento</div>
-				<div class="col-md-6" align="right">{$tipo_pgto}</div>
-			</div>
+<div class="row valores">
+	<div class="col-md-6">Forma de Pagamento</div>
+	<div class="col-md-6" align="right">' . $tipo_pgto . '</div>
+</div>
 
-			<div class="row valores">
-				<div class="col-md-6">Forma de Entrega</div>
-				<div class="col-md-6" align="right">{$entrega}</div>
-			</div>
+<div class="row valores">
+	<div class="col-md-6">Forma de Entrega</div>
+	<div class="col-md-6" align="right">' . $entrega . '</div>
+</div>
 
 
-			<div class="row valores">
-				<div class="col-md-6">Está Pago</div>
-				<div class="col-md-6" align="right"><b>{$pago}</b></div>
-			</div>
+<div class="row valores">
+	<div class="col-md-6">Está Pago</div>
+	<div class="col-md-6" align="right"><b>' . $pago . '</b></div>
+</div>
 
-			<div class="th" style="margin-bottom: 10px"></div>
+<div class="th" style="margin-bottom: 10px"></div>
 
-			HTML;
-			if ($entrega == "Delivery") {
-			echo <<<HTML
-				<div class="valores" align="center">
-				<b>Endereço para Entrega</b>
-				<br>
-				{$rua_cliente} {$numero_cliente} {$complemento_cliente}
-				{$bairro_cliente} {$cidade_cliente}
-				</div>
-				<div class="th" style="margin-bottom: 10px"></div>
-				HTML;
-				}
+HTML;
+if ($entrega == "Delivery") {
+echo '<div class="valores" align="center">';
+	echo '<b>Endereço para Entrega</b><br>';
+	echo $rua_cliente . ' ' . $numero_cliente . ' ' . $complemento_cliente . ' ' . $bairro_cliente . ' ' . $cidade_cliente;
+	echo '</div>';
+echo '<div class="th" style="margin-bottom: 10px"></div>';
+}
 
-				if ($obs != "") {
-				echo <<<HTML
-					<div class="valores" align="center">
-					<b>Observação do Pedido</b>
-					<br>
-					{$obs}
-					</div>
-					<div class="th" style="margin-bottom: 10px"></div>
-					HTML;
-					}
+if ($obs != "") {
+echo '<div class="valores" align="center">';
+	echo '<b>Observação do Pedido</b><br>';
+	echo $obs;
+	echo '</div>';
+echo '<div class="th" style="margin-bottom: 10px"></div>';
+}
 
-					?>
+?>
